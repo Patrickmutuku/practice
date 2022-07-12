b1c48c4b5239370361556aa507d1b06f8447b00d
Patrickmutuku <49222106+Patrickmutuku@users.noreply.github.com>
2022-07-12 13:32:42 +0300

    Changes

---
+++ arrays/associativearrays.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        //associative arrays
+        //They are arrays that contain named values.
+        //first method to create the array
+        $marks=array("Patrick"=>"A","John"=>"B","Jane"=>"C");
+
+        echo "Patrick's Grade is". $marks ["Patrick"]."<br>";
+        echo "John's Grade is ".$marks ["John"]."<br>";
+        echo "Jane's Grade is ".$marks ["Jane"]."<br>";
+
+        //the second method to create the associative array
+        $marks["Patrick"]="Excellent";
+        $marks["John"]="Very Good";
+        $marks["Jane"]="Good";
+
+        echo "Patrick has an".$marks["Patrick"]."grade<br>";
+        echo "John has an".$marks["John"]."grade<br>";
+        echo "Jane has an".$marks["Jane"]."grade<br>";
+        
+        ?>
+    </body>
+</html>
